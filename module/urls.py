fc68e8d5c599406c0b1ca1ae1eb7406b0aca0f9f
Dillon McCaffrey <dillon.mccaffrey@codeinstitute.net>
2024-12-05 21:33:08 +0000

    test

--- module/urls.py
+++ module/urls.py
@@ -4,6 +4,5 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('responses', views.responses, name='responses'),
+    path('<str:number>/', views.module, name='module'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- student_goals/settings.py
+++ student_goals/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'module',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,17 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+
+# The directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Additional locations the staticfiles app will traverse if the FileSystemFinder finder is enabled.
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
